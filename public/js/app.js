4b9e2e2f86e0b1957343fe44beb5c2ec973f8912
Peter Trana <peter.trana@jayway.com>
2015-10-02 16:08:56 +0200

    Add page navigation, transition and add event btn

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $(window).on('hashchange', selectView);
+
+    function selectView() {
+        console.info(window.location.hash);
+
+        showPage(window.location.hash.replace('#', ''));
+    }
+
+    function showPage(pageName) {
+        $('.page').removeClass('show');
+        $('.page[data-page="' + pageName + '"]').addClass('show');
+    }
+
+});
