75555819d36f4629a52ea154f514ad414854ce7e
Joacim Löwgren <joacim.lowgren@gmail.com>
2015-10-02 11:02:53 +0200

    Add template for index

--- server.js
+++ server.js
@@ -2,50 +2,50 @@
 
 var debug = require('debug')('express-lab:server');
 var http = require('http');
+var app = require('./lib/app');
+var server = http.createServer(app);
+var io = require('socket.io')(server);
 
 function environment() {
-    return process.env.NODE_ENV || 'development';
+  return process.env.NODE_ENV || 'development';
 }
 
 function isProduction() {
-    return environment() === 'production';
+  return environment() === 'production';
 }
 
-var app = require('./lib/app');
+// Set rendering engine
+app.set('view engine', 'ejs');
 
 // Make sure uncaught exceptions are logged on exit
 process.on('uncaughtException', function(err) {
-    console.log("Uncaught exception", err, err.stack);
-    process.exit(1);
+  console.log("Uncaught exception", err, err.stack);
+  process.exit(1);
 });
-
 
 // Utility function to show middleware and routes
 // DEBUG=express-lab* npm start
 function printRoutes() {
-    var middleware = app._router.stack.map(function(route) {
-        return route.route ?
-            route.route.path :
-            route.name + ': ' + route.regexp;
-    });
-    debug('Middleware');
-    debug('\n' + middleware.join('\n') + '\n');
+  var middleware = app._router.stack.map(function(route) {
+    return route.route ?
+      route.route.path :
+      route.name + ': ' + route.regexp;
+  });
+  debug('Middleware');
+  debug('\n' + middleware.join('\n') + '\n');
 }
 
 function start() {
-    var port = process.env.PORT || 3000;
+  var port = process.env.PORT || 3000;
 
-    printRoutes();
-    server.listen(port, function() {
-        console.log(environment().toUpperCase() + ' server started');
-        console.log('Port:', port);
-        console.log('URL:', 'http://localhost:' + port);
-    });
+  printRoutes();
+  server.listen(port, function() {
+    console.log(environment().toUpperCase() + ' server started');
+    console.log('Port:', port);
+    console.log('URL:', 'http://localhost:' + port);
+  });
 }
 
-
-var server = http.createServer(app);
-var io = require('socket.io')(server);
 app.setupWebsockets(io);
 
 server.app = app;
@@ -54,7 +54,6 @@ server.start = start;
 module.exports = server;
 
 // Start the app if called directly
-if(require.main === module)
-    start();
-
-
+if (require.main === module) {
+  start();
+}
